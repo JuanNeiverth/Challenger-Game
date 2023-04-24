9d7d2288718894344eef8105fe290cd12e10cb6f
JuanNeiverth <polli.neiverth@gmail.com>
2023-04-24 16:53:58 -0500

    Initial Commit

---
+++ Challenger Button and random/Challenger_Button_and_randomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenger_Button_and_randomApp.swift
+//  Challenger Button and random
+//
+//  Created by Juan on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Challenger_Button_and_randomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
